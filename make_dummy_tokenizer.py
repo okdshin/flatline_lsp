4a0ef75d08b817ad930dc403e48cb778f11177d5
okada <kokuzen@gmail.com>
2023-11-20 00:06:49 +0900

    fix make_dummy_tokenizer

--- make_dummy_tokenizer.py
+++ make_dummy_tokenizer.py
@@ -1,2 +1,4 @@
 import sentencepiece as spm
+from pathlib import Path
+Path("./dummy_tokenizer").mkdir(exist_ok=True)
 spm.SentencePieceTrainer.train(input="dummy_file", model_prefix='dummy_tokenizer/tokenizer', vocab_size=51200, byte_fallback=True)
