6f698dde8815a50a87f286ec9537f4c41b5bbc98
okada <kokuzen@gmail.com>
2023-11-26 00:49:04 +0900

    apply black format

--- flatline_lsp.py
+++ flatline_lsp.py
@@ -9,8 +9,13 @@ from lsprotocol import types as lsp
 from pygls.server import LanguageServer
 
 import torch
-from transformers import (AutoTokenizer, PreTrainedModel,
-                          PreTrainedTokenizer, PretrainedConfig, StoppingCriteria)
+from transformers import (
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+    PretrainedConfig,
+    StoppingCriteria,
+)
 from transformers.modeling_outputs import CausalLMOutput
 
 
@@ -19,16 +24,27 @@ class LlamaCppConfig(PretrainedConfig):  # type: ignore
 
 
 class LlamaCppCausalLM(PreTrainedModel):
-    def __init__(self, config: LlamaCppConfig, backend_server_host: str, backend_server_port: int, model_name: str, n_threads: int, n_gpu_layers: int):
+    def __init__(
+        self,
+        config: LlamaCppConfig,
+        backend_server_host: str,
+        backend_server_port: int,
+        model_name: str,
+        n_threads: int,
+        n_gpu_layers: int,
+    ):
         super().__init__(config)
 
         self.baseckend_server_host = backend_server_host
         self.baseckend_server_port = backend_server_port
         try:
-            requests.get(f"http://{self.baseckend_server_host}:{self.baseckend_server_port}")
+            requests.get(
+                f"http://{self.baseckend_server_host}:{self.baseckend_server_port}"
+            )
         except Exception:
             subprocess.Popen(
-                f"/home/okada/flatline2/build/bin/flatline-server --model-path {model_name} --n-gpu_layers {n_gpu_layers}".split())
+                f"/home/okada/flatline2/build/bin/flatline-server --model-path {model_name} --n-gpu_layers {n_gpu_layers}".split()
+            )
 
     @property
     def device(self) -> torch.device:
@@ -43,11 +59,15 @@ class LlamaCppCausalLM(PreTrainedModel):
         input_ids: torch.LongTensor,
         **kwargs,
     ) -> CausalLMOutput:
-        res = requests.post(f"http://{self.baseckend_server_host}:{self.baseckend_server_port}/v1/calc_next_token_logits",
-                            json=dict(input_tokens=input_ids[0].tolist()))
+        res = requests.post(
+            f"http://{self.baseckend_server_host}:{self.baseckend_server_port}/v1/calc_next_token_logits",
+            json=dict(input_tokens=input_ids[0].tolist()),
+        )
         return CausalLMOutput(
             loss=None,
-            logits=torch.FloatTensor(res.json()["next_token_logits"]).reshape(1, 1, 51200),
+            logits=torch.FloatTensor(res.json()["next_token_logits"]).reshape(
+                1, 1, 51200
+            ),
             hidden_states=None,
             attentions=None,
         )
@@ -69,12 +89,18 @@ class StopWord(StoppingCriteria):
         self.stop_tokens_len = len(tokenizer(stop_word).input_ids)
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> bool:
-        suffix_text = self.tokenizer.decode(input_ids[0][-self.stop_tokens_len:])
+        suffix_text = self.tokenizer.decode(input_ids[0][-self.stop_tokens_len :])
         return suffix_text.endswith(self.stop_word)
 
 
 class StopCutoffCompletion(StoppingCriteria):
-    def __init__(self, latest_completion_id: List[int], latest_completion_id_lock: threading.Lock, completion_id: int, lang_server: LanguageServer):
+    def __init__(
+        self,
+        latest_completion_id: List[int],
+        latest_completion_id_lock: threading.Lock,
+        completion_id: int,
+        lang_server: LanguageServer,
+    ):
         super().__init__()
         self.latest_completion_id = latest_completion_id
         self.latest_completion_id_lock = latest_completion_id_lock
@@ -85,19 +111,30 @@ class StopCutoffCompletion(StoppingCriteria):
         with self.latest_completion_id_lock:
             if self.latest_completion_id[0] != self.completion_id:
                 self.lang_server.show_message(
-                    f"stop-cutoff-completion {self.completion_id}", lsp.MessageType.Info)
+                    f"stop-cutoff-completion {self.completion_id}", lsp.MessageType.Info
+                )
                 return True
             else:
                 return False
 
 
 class LanguageModelForCompletion:
-    def __init__(self, lang_server: LanguageServer, max_new_tokens: int, backend_server_host: str, backend_server_port: int, model_name: str, n_threads: int, n_gpu_layers: int):
+    def __init__(
+        self,
+        lang_server: LanguageServer,
+        max_new_tokens: int,
+        backend_server_host: str,
+        backend_server_port: int,
+        model_name: str,
+        n_threads: int,
+        n_gpu_layers: int,
+    ):
         self.lang_server = lang_server
 
         assert model_name.endswith(".gguf")
         self.tokenizer = AutoTokenizer.from_pretrained(
-            "Salesforce/codegen25-7b-multi", trust_remote_code=True)
+            "Salesforce/codegen25-7b-multi", trust_remote_code=True
+        )
         self.model = LlamaCppCausalLM(
             config=LlamaCppConfig(),
             backend_server_host=backend_server_host,
@@ -128,11 +165,16 @@ class LanguageModelForCompletion:
             if stop_cutoff_completion():
                 return "<canceled>"
             tokenized_prompt = self.tokenizer(text).input_ids
-            generated_tokens = self.model.generate(inputs=torch.LongTensor(
-                [tokenized_prompt]), max_new_tokens=self.max_new_tokens, do_sample=False,
-                stopping_criteria=[stop_cutoff_completion, self.stop_word])[0]
+            generated_tokens = self.model.generate(
+                inputs=torch.LongTensor([tokenized_prompt]),
+                max_new_tokens=self.max_new_tokens,
+                do_sample=False,
+                stopping_criteria=[stop_cutoff_completion, self.stop_word],
+            )[0]
             # stopping_criteria=[stop_cutoff_completion])[0]
-            generated_text = self.tokenizer.decode(generated_tokens[len(tokenized_prompt):])
+            generated_text = self.tokenizer.decode(
+                generated_tokens[len(tokenized_prompt) :]
+            )
         return generated_text
 
 
@@ -145,17 +187,21 @@ server = LanguageServer("flatline-lsp", "v0.0")
 @server.thread()
 @server.feature(
     lsp.TEXT_DOCUMENT_COMPLETION,
-    lsp.CompletionOptions(trigger_characters=[
-                          ".", ",", " ", "(", ")", "[", "]", "{", "}"]),
+    lsp.CompletionOptions(
+        trigger_characters=[".", ",", " ", "(", ")", "[", "]", "{", "}"]
+    ),
 )
 def completions(
-        ls: LanguageServer, params: Optional[lsp.CompletionParams] = None) -> lsp.CompletionList:
+    ls: LanguageServer, params: Optional[lsp.CompletionParams] = None
+) -> lsp.CompletionList:
     # assert lm_for_completion is not None
     document = server.workspace.get_document(params.text_document.uri)
     line_index = params.position.line
     character_index = params.position.character
-    prompt = "".join(list(document.lines[max(0, line_index-15):line_index]) +
-                     [document.lines[line_index][:character_index]])
+    prompt = "".join(
+        list(document.lines[max(0, line_index - 15) : line_index])
+        + [document.lines[line_index][:character_index]]
+    )
 
     if lm_for_completion is None:
         completed_text = "<flatline_lsp_lm_for_completion is not initialized>"
@@ -164,10 +210,14 @@ def completions(
 
     return lsp.CompletionList(
         is_incomplete=True,
-        items=[lsp.CompletionItem(label="(FL)"+completed_text,
-                                  insert_text=completed_text,
-                                  insert_text_mode=lsp.InsertTextMode.AdjustIndentation,
-                                  documentation=completed_text)],
+        items=[
+            lsp.CompletionItem(
+                label="(FL)" + completed_text,
+                insert_text=completed_text,
+                insert_text_mode=lsp.InsertTextMode.AdjustIndentation,
+                documentation=completed_text,
+            )
+        ],
     )
 
 
@@ -175,8 +225,11 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--backend-server-host", type=str, default="localhost")
     parser.add_argument("--backend-server-port", type=int, default=5000)
-    parser.add_argument("--model-name", type=str,
-                        default="/home/okada/flatline2/codegen25-7b-multi/ggml-model-Q4_K.gguf")
+    parser.add_argument(
+        "--model-name",
+        type=str,
+        default="/home/okada/flatline2/codegen25-7b-multi/ggml-model-Q4_K.gguf",
+    )
     parser.add_argument("--max-new-tokens", type=int, default=256)
     parser.add_argument("--n-threads", type=int, default=8)
     parser.add_argument("--n-gpu-layers", type=int, default=32)
